01f816a056d67ccb9c3ec1f82ebc2b5be674dd6e
crazyfire <fdff87554@gmail.com>
2021-01-29 14:39:11 +0800

    finish basic color change testing code

--- codetest/colorTest/colortransfer/colortransfer.go
+++ codetest/colorTest/colortransfer/colortransfer.go
@@ -1,7 +1,11 @@
 package colortransfer
 
-import "image"
+import (
+	"image"
+	"image/color"
+)
 
+//ColorToGray convert image color to Grayscale
 func ColorToGray(img image.Image) image.Image {
 	grayImg := image.NewGray(img.Bounds())
 	for y := img.Bounds().Min.Y; y < img.Bounds().Max.Y; y++ {
@@ -13,13 +17,67 @@ func ColorToGray(img image.Image) image.Image {
 	return grayImg
 }
 
-//func ColorToYCbCr(img image.Image, ycbcrRatio int) image.Image {
-//	ycbcrImg := image.NewYCbCr(img.Bounds(), image.YCbCrSubsampleRatio(ycbcrRatio))
+//ColorToAlpha convert image color to Alpha
+func ColorToAlpha(img image.Image) image.Image {
+	alphaImg := image.NewAlpha(img.Bounds())
+	for y := img.Bounds().Min.Y; y < img.Bounds().Max.Y; y++ {
+		for x := img.Bounds().Min.X; x < img.Bounds().Max.X; x++ {
+			alphaImg.Set(x, y, img.At(x, y))
+		}
+	}
+
+	return alphaImg
+}
+
+//ColorToCMYK convert image color to CMYK
+func ColorToCMYK(img image.Image) image.Image {
+	CMYKImg := image.NewCMYK(img.Bounds())
+	for y := img.Bounds().Min.Y; y < img.Bounds().Max.Y; y++ {
+		for x := img.Bounds().Min.X; x < img.Bounds().Max.X; x++ {
+			CMYKImg.Set(x, y, img.At(x, y))
+		}
+	}
+
+	return CMYKImg
+}
+
+//ColorToNRGBA convert image color to NRGBA
+func ColorToNRGBA(img image.Image) image.Image {
+	NRGBAImg := image.NewNRGBA(img.Bounds())
+	for y := img.Bounds().Min.Y; y < img.Bounds().Max.Y; y++ {
+		for x := img.Bounds().Min.X; x < img.Bounds().Max.X; x++ {
+			NRGBAImg.Set(x, y, img.At(x, y))
+		}
+	}
+
+	return NRGBAImg
+}
+
+////ColorToPaletted convert image color to Paletted
+//func ColorToPaletted(img image.Image) image.Image {
+//	PalettedImg := image.NewPaletted(img.Bounds(), )
 //	for y := img.Bounds().Min.Y; y < img.Bounds().Max.Y; y++ {
 //		for x := img.Bounds().Min.X; x < img.Bounds().Max.X; x++ {
-//			ycbcrImg.Set(x, y, img.At(x, y))
+//			PalettedImg.Set(x, y, img.At(x, y))
 //		}
 //	}
 //
-//	return ycbcrImg
+//	return PalettedImg
 //}
+
+func ColorToYCbCr(img image.Image, ycbcrRatio int) image.Image {
+	ycbcrImg := image.NewYCbCr(img.Bounds(), image.YCbCrSubsampleRatio(ycbcrRatio))
+	for y := img.Bounds().Min.Y; y < img.Bounds().Max.Y; y++ {
+		for x := img.Bounds().Min.X; x < img.Bounds().Max.X; x++ {
+			r, g, b, _ := img.At(x, y).RGBA()
+			y_, cb, cr := color.RGBToYCbCr(uint8(r), uint8(g), uint8(b))
+
+			ycbcrImg.Y[ycbcrImg.YOffset(x, y)] = y_
+			ycbcrImg.Cb[ycbcrImg.COffset(x, y)] = cb
+			ycbcrImg.Cr[ycbcrImg.COffset(x, y)] = cr
+		}
+	}
+
+	return ycbcrImg
+}
+
--- codetest/colorTest/main.go
+++ codetest/colorTest/main.go
@@ -1,11 +1,74 @@
 package main
 
 import (
+	"errors"
+	"fmt"
 	"github.com/fdff87554/watermark_tools/tree/main/codetest/colorTest/colortransfer"
+	"image"
 	"image/png"
 	"log"
 	"os"
 )
+
+type ColorSamples int
+
+const (
+	Gray ColorSamples = iota
+	Alpha
+	CMYK
+	NRGBA
+	YCbCr
+	//Paletted
+)
+
+func imageTranfer(img image.Image, colorType int) error {
+	writer, err := os.Create(fmt.Sprintf("../testImage/output/transfer%d.png", colorType))
+	if err != nil {
+		return err
+	}
+	defer writer.Close()
+
+	switch ColorSamples(colorType) {
+	case Gray:
+		img := colortransfer.ColorToGray(img)
+		if err := png.Encode(writer, img); err != nil {
+			log.Fatal(err)
+		}
+		return nil
+	case Alpha:
+		img := colortransfer.ColorToAlpha(img)
+		if err := png.Encode(writer, img); err != nil {
+			log.Fatal(err)
+		}
+		return nil
+	case CMYK:
+		img := colortransfer.ColorToCMYK(img)
+		if err := png.Encode(writer, img); err != nil {
+			log.Fatal(err)
+		}
+		return nil
+	case NRGBA:
+		img := colortransfer.ColorToNRGBA(img)
+		if err := png.Encode(writer, img); err != nil {
+			log.Fatal(err)
+		}
+		return nil
+	case YCbCr:
+		img := colortransfer.ColorToYCbCr(img, 0)
+		if err := png.Encode(writer, img); err != nil {
+			log.Fatal(err)
+		}
+		return nil
+	//case Paletted:
+	//	img := colortransfer.ColorToPaletted(img)
+	//	if err := png.Encode(writer, img); err != nil {
+	//		log.Fatal(err)
+	//	}
+	//	return nil
+	default:
+		return errors.New("none define type")
+	}
+}
 
 func main() {
 	pngReader, err := os.Open("../testImage/cat_in_png.png")
@@ -13,19 +76,24 @@ func main() {
 		log.Fatal(err)
 	}
 	defer pngReader.Close()
-
 	pngImg, err := png.Decode(pngReader)
 	if err != nil {
 		log.Fatal(err)
 	}
-	grayImg := colortransfer.ColorToGray(pngImg)
-	grayWriter, err := os.Create("../testImage/transferGray.png")
-	if err != nil {
+
+	if err := imageTranfer(pngImg, 0); err != nil {
 		log.Fatal(err)
 	}
-	defer grayWriter.Close()
-
-	if err := png.Encode(grayWriter, grayImg); err != nil {
+	if err := imageTranfer(pngImg, 1); err != nil {
+		log.Fatal(err)
+	}
+	if err := imageTranfer(pngImg, 2); err != nil {
+		log.Fatal(err)
+	}
+	if err := imageTranfer(pngImg, 3); err != nil {
+		log.Fatal(err)
+	}
+	if err := imageTranfer(pngImg, 4); err != nil {
 		log.Fatal(err)
 	}
 }
